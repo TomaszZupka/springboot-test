b7bc1057d541443bb734401108e44a68c9e3e46d
Tomasz Zupka <tzupka@apreel.com>
2020-01-30 16:07:41 +0100

    controller

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Test {
+    @GetMapping("/")
+    String test() {
+        return "hello worlda";
+    }
+
+    @GetMapping("/test")
+    String test1() {
+        return "hello world anothera";
+    }
+}
